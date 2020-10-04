2155f2f892e4a732f86e74036cc272b8c8d7c642
n.ermolenko <n.ermolenko@rt-solar.ru>
2020-10-04 15:04:54 +0300

    add solution for 02-components 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,6 +1,27 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: `<button type="button"
+    @click="onButtonClick">{{ count }}</button>`,
+
+  props: {
+    count: {
+      type: Number,
+      requires: false,
+      default: 0,
+      validator: value => typeof value === 'number',
+    },
+  },
+
+  methods: {
+    onButtonClick () {
+      this.$emit('increment', this.count + 1); // DO NOT USE this.count++
+    },
+  },
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
 
   // Компонент должен иметь входной параметр
 
