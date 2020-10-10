7c4b37eb0394c23269be981e931507043150b6a1
n.ermolenko <n.ermolenko@rt-solar.ru>
2020-10-10 12:41:09 +0300

    add solution for 04-SPA 01-ContentTabs

--- 04-SPA/01-ContentTabs/components/ContentTabs.vue
+++ 04-SPA/01-ContentTabs/components/ContentTabs.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="content-tabs">
     <div class="content-tabs__nav">
-      <a href="#" class="content-tabs__tab">Page A</a>
-      <a href="#" class="content-tabs__tab">Page B</a>
+      <router-link
+        v-for="{ to, text } in tabs"
+        :key="to.name || to"
+        :to="to"
+        active-class="content-tabs__tab_active"
+        class="content-tabs__tab">{{ text }}</router-link>
     </div>
-    <div class="content-tabs__content">Content</div>
+    
+    <div class="content-tabs__content">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ContentTabs',
+
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+      validator: value => value.every(item => Object.keys(item).includes('to')),
+    },
+  },
 };
 </script>
 
