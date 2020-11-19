5f4f6b72e51d895055896d6bf040532052be6296
n.ermolenko <n.ermolenko@rt-solar.ru>
2020-11-19 23:44:27 +0300

    add solution for 07-forms 01-CounterButton_last_final

--- 07-forms/01-CounterButton_last_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_last_final/components/CounterButton.vue
@@ -1,9 +1,44 @@
 <template>
-  <button></button>
+  <button @click="onButtonClick">{{ value }}</button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'counter-button',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  methods: {
+    onButtonClick() {
+      this.value = this.value + 1;
+      this.$emit('update:count', this.value); // Для <counter-button :count.sync="count4" />
+      this.$emit('increment', this.value);
+    },
+  },
+
+  data: () => ({
+    value: 0,
+  }),
+
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+        this.value = newValue;
+      },
+    },
+  },
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+};
 </script>
 
 <style>
